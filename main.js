6764cab66dd4bfc985531e8b68eb275058254528
jack morris <jack.morris@livetilesglobal.com>
2021-09-24 19:28:48 +0100

    init commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// Mobile Menu function
+function openMenu(){
+    const menu = document.getElementById("menuItems");
+    if(menu.style.display === "block"){
+        menu.style.display = "none";
+    }else{
+        menu.style.display = "block";
+    }
+}
